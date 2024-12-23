def74aee1d928ad84d69b3eebc77e454cd5ad8d7
Jakub Stibůrek <jakub@stiburek.dev>
2024-12-23 18:46:35 +0100

    chore(api): delete boilerplate

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AudioModule } from './audio/audio.module';
 
 @Module({
   imports: [AudioModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
