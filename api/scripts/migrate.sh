71b9f3f6c3c9511c59d8742c0cd643a5a7e5cb9b
Jakub Stibůrek <jakub@stiburek.dev>
2025-01-01 19:10:57 +0100

    ci: test workflows

--- api/scripts/migrate.sh
+++ api/scripts/migrate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 migrate -path migrations -database "postgresql://postgres:password@localhost:5432/deep_cord?sslmode=disable" "$@"
+
+# migrate -path migrations -database "postgresql://deep_cord_owner:L2kfMAFCOT8z@ep-winter-credit-a2joxklf.eu-central-1.aws.neon.tech/deep_cord?sslmode=require" "$@"
