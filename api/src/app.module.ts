de2a04991ff9641dd11ca4109e54d8531bce6a17
Jakub Stibůrek <jakub@stiburek.dev>
2025-01-12 18:46:40 +0100

    feat(api): http logging

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './common/config/configuration';
 import { CoreModule } from './modules/core/core.module';
 import { HealthModule } from './modules/health/health.module';
+import { HttpReqResLoggingMiddleware } from './common/middleware/http-req-res-logging.middleware';
 
 @Module({
   imports: [
@@ -14,4 +15,8 @@ import { HealthModule } from './modules/health/health.module';
   ],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(HttpReqResLoggingMiddleware).forRoutes('*'); // Apply the middleware to all routes
+  }
+}
---
+++ api/src/common/middleware/http-req-res-logging.middleware.ts
@@ -0,0 +1,33 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import type { NextFunction, Request, Response } from 'express';
+import { DateTime, Interval } from 'luxon';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class HttpReqResLoggingMiddleware implements NestMiddleware {
+  logger = new Logger('HTTP');
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const { method, originalUrl, ip } = req;
+    const start = DateTime.now();
+
+    res.on('finish', () => {
+      const end = DateTime.now();
+      const duration = Interval.fromDateTimes(start, end)
+        .toDuration('milliseconds')
+        .toHuman({ unitDisplay: 'short' });
+      const message = {
+        id: uuid(),
+        method,
+        resource: originalUrl,
+        status: res.statusCode,
+        duration: `${duration}`,
+        ip,
+      };
+
+      this.logger.log(message);
+    });
+
+    next();
+  }
+}
