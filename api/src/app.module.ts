7bf13ec7bfd070851eea2978ec36cfd086f3cda5
Jakub Stibůrek <jakub@stiburek.dev>
2025-01-30 12:46:32 +0100

    feat(api): serve static files from uploads

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,11 +4,16 @@ import configuration from './common/config/configuration';
 import { CoreModule } from './modules/core/core.module';
 import { HealthModule } from './modules/health/health.module';
 import { HttpReqResLoggingMiddleware } from './common/middleware/http-req-res-logging.middleware';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../uploads/'),
     }),
     CoreModule,
     HealthModule,
